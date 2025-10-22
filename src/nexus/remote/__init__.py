d351bc0c8b80e94da822424e386841cb3648403e
windoliver <deepbuild@all-hands.dev>
2025-10-22 14:34:36 -0700

    Improve error handling in remote client #195

--- src/nexus/remote/__init__.py
+++ src/nexus/remote/__init__.py
@@ -4,6 +4,16 @@ This module provides a remote client implementation of NexusFilesystem
 that connects to a Nexus RPC server over HTTP.
 """
 
-from nexus.remote.client import RemoteNexusFS
+from nexus.remote.client import (
+    RemoteConnectionError,
+    RemoteFilesystemError,
+    RemoteNexusFS,
+    RemoteTimeoutError,
+)
 
-__all__ = ["RemoteNexusFS"]
+__all__ = [
+    "RemoteNexusFS",
+    "RemoteFilesystemError",
+    "RemoteConnectionError",
+    "RemoteTimeoutError",
+]

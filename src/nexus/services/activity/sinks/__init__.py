2977161aeac52608ddf5faffae60dc9a7a2736b3
windoliver <taofeng.nju@gmail.com>
2026-04-30 16:18:56 -0700

    feat(#3791): sink protocol + NoopSink + RecordingSink

---
+++ src/nexus/services/activity/sinks/__init__.py
@@ -0,0 +1,5 @@
+from nexus.services.activity.sinks.noop import NoopSink
+from nexus.services.activity.sinks.protocol import SinkProtocol
+from nexus.services.activity.sinks.recording import RecordingSink
+
+__all__ = ["NoopSink", "RecordingSink", "SinkProtocol"]

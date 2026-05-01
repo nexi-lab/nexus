b97a4637ae5c0d807ee75221ae61299636656844
windoliver <taofeng.nju@gmail.com>
2026-05-01 14:40:07 -0700

    test: add profile matrix helpers

--- tests/testkit/__init__.py
+++ tests/testkit/__init__.py
@@ -4,6 +4,7 @@ from tests.testkit.auth import TEST_ADMIN_CONTEXT, TEST_CONTEXT, operation_conte
 from tests.testkit.backends import FailingBackend
 from tests.testkit.metadata import DictMetastore, FailingMetastore, InMemoryNexusFS, MetastoreError
 from tests.testkit.nexus_factory import make_test_nexus
+from tests.testkit.profiles import TestProfile, profile_matrix, pytest_profile_params
 from tests.testkit.records import InMemoryRecordStore
 from tests.testkit.websocket import MockWebSocket
 
@@ -17,6 +18,9 @@ __all__ = [
     "MockWebSocket",
     "TEST_ADMIN_CONTEXT",
     "TEST_CONTEXT",
+    "TestProfile",
     "make_test_nexus",
     "operation_context",
+    "profile_matrix",
+    "pytest_profile_params",
 ]
